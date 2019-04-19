1595fb0d423e509b755567efe81cebc30192a269
risyandi <risyandi.94@gmail.com>
2019-04-19 22:30:03 +0700

    modified

--- ads labs/parallax-template/Javascript_Experiment.js
+++ ads labs/parallax-template/Javascript_Experiment.js
@@ -1,73 +1,73 @@
 // experiment - Risyandi 2018
 // trick events with onclick
-document.getElementById("commandOne").onclick = allCallFunc;
-document.getElementById("commandTwo").onclick = allDeleteFunc;
+// document.getElementById("commandOne").onclick = allCallFunc;
+// document.getElementById("commandTwo").onclick = allDeleteFunc;
 
 // trick events with event listener
 // document.querySelector('#commandOne').addEventListener('click', allCallFunc);
 // document.querySelector('#commandTwo').addEventListener('click', allDeleteFunc);
 
 // call all function in one function
-function allCallFunc() {
+// function allCallFunc() {
 // commandOnePride();
 // commandTwoPride();
 // listCookies();
 // curry(5);
-}
+// }
 
 // delete result and console
-function allDeleteFunc() {
-  console.clear();
-  document.getElementById('result').innerHTML = "";
-}
+// function allDeleteFunc() {
+//   console.clear();
+//   document.getElementById('result').innerHTML = "";
+// }
 
 // function command one
-function commandOnePride() {
-  const PI = 3.14;
-  PI > 3.0;
-  document.getElementById('result').innerHTML = PI ;
-  console.log("I'm Pressed One Pride");
-}
+// function commandOnePride() {
+//   const PI = 3.14;
+//   PI > 3.0;
+//   document.getElementById('result').innerHTML = PI ;
+//   console.log("I'm Pressed One Pride");
+// }
 
 // function command two 
-function commandTwoPride() {
-  console.log("I'm Pressed Two Pride");
-}
+// function commandTwoPride() {
+//   console.log("I'm Pressed Two Pride");
+// }
 
 // create function to collect and listing a cookies on your browser
-function listCookies() {
-  var theCookies = document.cookie.split(';');
-  var asString = '';
-  for (let index = 0; index <= theCookies.length; index++) {
-    asString += index + ' ' + theCookies[index-1] + "\n";   
-  }
-  console.log(asString, "cek astring");
-  return asString;
-}
+// function listCookies() {
+//   var theCookies = document.cookie.split(';');
+//   var asString = '';
+//   for (let index = 0; index <= theCookies.length; index++) {
+//     asString += index + ' ' + theCookies[index-1] + "\n";   
+//   }
+//   console.log(asString, "cek astring");
+//   return asString;
+// }
 
 // create function hash maps without side effect
-function hashMaps() {
-    const dirtyMap = {};
-    console.log(dirtyMap, "dirty map"); // have a object
+// function hashMaps() {
+//     const dirtyMap = {};
+//     console.log(dirtyMap, "dirty map"); // have a object
 
-    const cleanMap = Object.create(null);
-    console.log(cleanMap, "clean map"); // not have a object
+//     const cleanMap = Object.create(null);
+//     console.log(cleanMap, "clean map"); // not have a object
 
-    dirtyMap.constructor
-    cleanMap.constructor
+//     dirtyMap.constructor
+//     cleanMap.constructor
 
-    const key = 0;
-    for (key in dirtyMap) {
-        if (dirtyMap.hasOwnProperty(key)) {
-            console.log(key + "->" + dirtyMap[key]);
-        }
-    }
+//     const key = 0;
+//     for (key in dirtyMap) {
+//         if (dirtyMap.hasOwnProperty(key)) {
+//             console.log(key + "->" + dirtyMap[key]);
+//         }
+//     }
 
-    for (key in cleanMap) {
-        console.log(key + "->" + cleanMap[key]);
-    }
+//     for (key in cleanMap) {
+//         console.log(key + "->" + cleanMap[key]);
+//     }
 
-}
+// }
 
 
 /**
@@ -131,3 +131,11 @@ function hashMaps() {
     plus5(10) // output -> 15
 
  */
+/**
+ * append a element on middle
+ */
+var numMiddle = Math.ceil($(".setpage p").length / 2);
+var numLength = Math.ceil($(".setpage p").length);
+console.log(numMiddle, ":value middle");
+console.log(numLength, ":value length");
+$(".setpage p:nth-child(" + numMiddle + ")").append("<h1>hallo ini iklan</h1>");
